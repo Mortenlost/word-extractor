9ccfb8702eb3d7840919e15505a9f1d0e4b3a3e3
Mortenlost <musicaarhe@gmail.com>
2020-11-16 17:58:57 -0600

    update

--- src/main/index.js
+++ src/main/index.js
@@ -6,6 +6,8 @@ function createWindow () {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
+    minWidth: 800,
+    minHeight: 600,
     webPreferences: {
       nodeIntegration: true
     }
