05e0a2078af9b85a4dfc938417af3d8fa4ac3a78
Mortenlost <musicaarhe@gmail.com>
2020-11-20 17:42:47 -0600

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+import * as validUrl from "valid-url"; 
 
 const bg = require("./bg.jpg"); 
 
 export const App = () => {
+    const refUrl = useRef(null);
+    const refLetter = useRef(null);
+    const [valid, setValid] = useState(false);
     return (
         <div>
             <div className="fixed top-0 left-0 w-screen h-screen z-0">
@@ -13,8 +17,14 @@ export const App = () => {
                     Word Extractor
             </h1>
                 <form className="mt-5">
-                    <input className="w-full mb-2 rounded p-2 text-lg text-gray-700 placeholder-gray-700 font focus:outline-none" type="text" name="url" placeholder="URL" />
-                    <input className="w-full mb-2 rounded p-2 text-lg text-gray-700 placeholder-gray-700 font focus:outline-none" type="text" name="letter" placeholder="Letter" />
+                    <input onChange={(e) => { 
+                        if (validUrl.isWebUri(e.target.value)) {
+                            setValid(true); 
+                        } else { 
+                            setValid(false);
+                        }
+                    }} ref={refUrl} className={`w-full mb-2 rounded p-2 text-lg text-gray-700 placeholder-gray-700 font focus:outline-none ${valid ? "" : "border-2 border-red-600"}`} type="text" name="url" placeholder="URL" />
+                    <input ref={refLetter} className="w-full mb-2 rounded p-2 text-lg text-gray-700 placeholder-gray-700 font focus:outline-none" type="text" name="letter" placeholder="Letter" />
                     <button className="py-2 px-6 bg-teal-400 text-teal-900 font-bold text-xl rounded focus:outline-none" type="submit">Get Words</button>
                 </form>
             </div>
