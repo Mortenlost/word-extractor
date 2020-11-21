5ff83caccd398570746da756022c96cbd225837b
Mortenlost <musicaarhe@gmail.com>
2020-11-21 12:05:38 -0600

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useRef, useState } from "react";
-import * as validUrl from "valid-url"; 
+import * as validUrl from "valid-url";
 
-const bg = require("./bg.jpg"); 
+const bg = require("./bg.jpg");
 
 export const App = () => {
     const refUrl = useRef(null);
@@ -15,19 +15,46 @@ export const App = () => {
             <div className="fixed top-0 left-0 w-screen h-screen z-10 pl-5">
                 <h1 className="text-white text-5xl font font-black text-shadow-black">
                     Word Extractor
-            </h1>
+                </h1>
                 <form className="mt-5">
-                    <input onChange={(e) => { 
-                        if (validUrl.isWebUri(e.target.value)) {
-                            setValid(true); 
-                        } else { 
-                            setValid(false);
-                        }
-                    }} ref={refUrl} className={`w-full mb-2 rounded p-2 text-lg text-gray-700 placeholder-gray-700 font focus:outline-none ${valid ? "" : "border-2 border-red-600"}`} type="text" name="url" placeholder="URL" />
-                    <input ref={refLetter} className="w-full mb-2 rounded p-2 text-lg text-gray-700 placeholder-gray-700 font focus:outline-none" type="text" name="letter" placeholder="Letter" />
-                    <button className="py-2 px-6 bg-teal-400 text-teal-900 font-bold text-xl rounded focus:outline-none" type="submit">Get Words</button>
+                    <input
+                        onChange={(e) => {
+                            if (validUrl.isWebUri(e.target.value)) {
+                                setValid(true);
+                            } else {
+                                setValid(false);
+                            }
+                        }}
+                        ref={refUrl}
+                        className={`w-full mb-2 rounded p-2 text-lg text-gray-700 placeholder-gray-700 font focus:outline-none ${
+                            valid ? "" : "border-2 border-red-600"
+                        }`}
+                        type="text"
+                        name="url"
+                        placeholder="URL"
+                    />
+                    <input
+                        onChange={(e) => {
+                            if (!e.target.validity.valid) {
+                                e.target.value = "";
+                            }
+                        }}
+                        ref={refLetter}
+                        className="w-full mb-2 rounded p-2 text-lg text-gray-700 placeholder-gray-700 font focus:outline-none"
+                        type="text"
+                        name="letter"
+                        placeholder="Letter"
+                        maxLength="1"
+                        pattern="[a-z]*"
+                    />
+                    <button
+                        className="py-2 px-6 bg-teal-400 text-teal-900 font-bold text-xl rounded focus:outline-none"
+                        type="submit"
+                    >
+                        Get Words
+                    </button>
                 </form>
             </div>
         </div>
     );
-}
+};
--- src/electron.js
+++ src/electron.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow } = require("electron");
 const path = require("path");
 const isDev = require("electron-is-dev");
 
@@ -11,28 +11,26 @@ function createWindow() {
         minWidth: 800,
         minHeight: 600,
         webPreferences: {
-            nodeIntegration: true
-        }
-    })
+            nodeIntegration: true,
+        },
+    });
 
     win.loadURL(
-        isDev
-            ? "http://localhost:3000"
-            : `file://${path.join(__dirname, "../build/index.html")}`
+        isDev ? "http://localhost:3000" : `file://${path.join(__dirname, "../build/index.html")}`
     );
-    win.webContents.openDevTools()
+    win.webContents.openDevTools();
 }
 
-app.whenReady().then(createWindow)
+app.whenReady().then(createWindow);
 
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit()
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+        app.quit();
     }
-})
+});
 
-app.on('activate', () => {
+app.on("activate", () => {
     if (BrowserWindow.getAllWindows().length === 0) {
-        createWindow()
+        createWindow();
     }
-})
+});
